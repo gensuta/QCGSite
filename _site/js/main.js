0264445a972b928bd391727fd96d5eb89dec635c
Geneva <47046378+gensuta@users.noreply.github.com>
2023-08-14 20:43:09 -0400

    Added new pages and started inserting images

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,21 +1,46 @@
-//code taken from w3 schools
+
 
 let backtoTopBtn = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-//window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-//     mybutton.style.display = "block";
-//   } else {
-//     mybutton.style.display = "none";
-//   }
-// }
 
 function topFunction() {
     document.getElementById("top").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
-
-    // document.body.scrollTop = 0; // For Safari
-    // document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
+
+   // Add slideDown animation to Bootstrap dropdown when expanding.
+   $('.dropdown').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+  });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $('.dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+  });
+
+
+
+  const elements = document.querySelectorAll(".hidden-line");
+
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+  function isElementVisible() {
+    for (card of elements) {
+      isElementInViewport(card)
+        ? card.classList.add("animated-line")
+        : card.classList.remove("animated-line");
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", isElementVisible);
+  window.addEventListener("scroll", isElementVisible);
+  window.addEventListener("resize", isElementVisible);
+  
